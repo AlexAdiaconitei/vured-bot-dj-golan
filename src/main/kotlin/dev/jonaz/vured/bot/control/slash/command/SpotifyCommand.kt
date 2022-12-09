3533863c3d9474ad15a24a17f98b5b6a87702876
AlexAdiaconitei <alexibs98@gmail.com>
2022-12-09 20:06:43 +0100

    update version - 2.2.1-0.2.0

---
+++ src/main/kotlin/dev/jonaz/vured/bot/control/slash/command/SpotifyCommand.kt
@@ -0,0 +1,37 @@
+package dev.jonaz.vured.bot.control.slash.command
+
+import dev.jonaz.vured.bot.application.Translation
+import dev.jonaz.vured.bot.control.slash.CommandHandler
+import dev.jonaz.vured.bot.control.slash.SlashCommand
+import dev.jonaz.vured.bot.service.music.MusicService
+import dev.jonaz.vured.bot.util.extensions.genericInject
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.requests.restaction.CommandCreateAction
+
+@SlashCommand(
+    name = "spotify",
+    description = Translation.COMMAND_SPOTIFY_DESC
+)
+class SpotifyCommand : CommandHandler {
+    private val musicService by genericInject<MusicService>()
+
+    override fun upsertCommand(commandCreateAction: CommandCreateAction): CommandCreateAction {
+        return commandCreateAction.addOption(
+            OptionType.STRING,
+            Translation.COMMAND_SPOTIFY_OPTION_NAME,
+            Translation.COMMAND_SPOTIFY_OPTION_DESC,
+            true
+        )
+    }
+
+    override fun execute(event: SlashCommandInteractionEvent) {
+        val query = event.getOption("query")?.asString
+
+        query?.run {
+            musicService.loadItem(event.member, "spsearch:$this")
+        }
+
+        event.hook.sendMessage(Translation.COMMAND_SPOTIFY_RESPONSE).setEphemeral(true).queue()
+    }
+}
